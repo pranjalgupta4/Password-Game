51fdc341e407aecacd163a5dec2709ed10faa2ae
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-01-14 15:50:01 +0530

    updated Rule10 and other css changes

--- src/Component/Rules.jsx
+++ src/Component/Rules.jsx
@@ -1,8 +1,10 @@
-import style from "./Rules.module.css";
 import Rule from "./Rule";
 import pepsi from "../../public/Sponsers/pepsi.svg";
 import shell from "../../public/Sponsers/shell.svg";
 import starbucks from "../../public/Sponsers/starbucks.svg";
+import refresh from "../../public/refresh.svg";
+import defaultCaptcha from "../../public/default-captcha.png";
+import styles from "./Rules.module.css";
 // import googleMapList from "./RulesList/googleMapList";
 
 export default function Rules() {
@@ -14,9 +16,16 @@ export default function Rules() {
         index="8"
       >
         <div>
-          <img src={pepsi} alt="pepsi" />
-          <img src={shell} alt="shell" />
-          <img src={starbucks} alt="starbucks" />
+          <img
+            src={defaultCaptcha}
+            alt="Image Captcha"
+            className={styles["captcha-img"]}
+          />
+          <img
+            src={refresh}
+            alt="refresh"
+            className={styles["captcha-refresh"]}
+          />
         </div>
       </Rule>
       <Rule
@@ -24,6 +33,17 @@ export default function Rules() {
         description="Your password must include one of our sponsors:"
         index="8"
       />
+      <Rule
+        status={true}
+        index="8"
+        description="Your password must include one of our sponsors:"
+      >
+        <div>
+          <img src={pepsi} alt="pepsi" />
+          <img src={shell} alt="shell" />
+          <img src={starbucks} alt="starbucks" />
+        </div>
+      </Rule>
     </>
   );
 }
--- src/Component/RulesList/RulesList.jsx
+++ src/Component/RulesList/RulesList.jsx
@@ -2,21 +2,24 @@ import Rule from "../Rule";
 import pepsi from "../../public/Sponsers/pepsi.svg";
 import shell from "../../public/Sponsers/shell.svg";
 import starbucks from "../../public/Sponsers/starbucks.svg";
+import refresh from "../../public/refresh.svg";
+import defaultCaptcha from "../../public/default-captcha.png";
+import { useState, useEffect, useCallback } from "react";
 // import googleMapList from "./googleMapList";
 
-const rulesList = [
-  function rule2(input) {
+const RulesList = [
+  function Rule2(input) {
     const reg = /\d/g;
     return (
       <Rule
-        status={input.match(/\d/g) ? true : false}
+        status={input.match(reg) ? true : false}
         index="2"
         description="Your password must include a number."
       />
     );
   },
-  function rule4(input) {
-    const reg = /\w|\s/g;
+  function Rule4(input) {
+    const reg = /[^\w\s]/g;
     return (
       <Rule
         status={input.match(reg) ? false : true}
@@ -25,7 +28,7 @@ const rulesList = [
       />
     );
   },
-  function rule6(input) {
+  function Rule6(input) {
     const reg =
       /(January|February|March|April|May|June|July|August|September|October|November|December)/gi;
     return (
@@ -36,7 +39,7 @@ const rulesList = [
       />
     );
   },
-  function rule8(input) {
+  function Rule8(input) {
     const reg = /(pepsi|starbucks|shell)/gi;
     return (
       <Rule
@@ -52,18 +55,54 @@ const rulesList = [
       </Rule>
     );
   },
-  function rule10(input) {
-    const reg =
-      /(January|February|March|April|May|June|July|August|September|October|November|December)/gi;
+
+  function Rule10(input) {
+    const [imgUrl, setImgUrl] = useState(defaultCaptcha);
+    const [captchaText, setCaptchaText] = useState("WCf6Uc");
+
+    const captchaHandler = useCallback(async function () {
+      const url =
+        "https://captcha-generator.p.rapidapi.com/?noise_number=10&fontname=sora";
+      const options = {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "e19ed6da22msh90d963871b5a9c5p1a90ffjsna8b019130de4",
+          "x-rapidapi-host": "captcha-generator.p.rapidapi.com",
+        },
+      };
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        const finalResult = JSON.parse(result);
+        setImgUrl(finalResult.image_url);
+        setCaptchaText(finalResult.solution);
+      } catch (error) {
+        console.error(error);
+      }
+    }, []);
+
+    useEffect(() => {
+      captchaHandler();
+    }, [captchaHandler]);
+
+    const reg = new RegExp(captchaText);
     return (
       <Rule
         status={input.match(reg) ? true : false}
         index="10"
         description="Your password must include this CAPTCHA:"
-      />
+      >
+        <div>
+          <img src={imgUrl} alt="Image Captcha" />
+          {/*add classname={styles["captcha-img"]} */}
+          <img src={refresh} alt="refresh" onClick={captchaHandler} />
+          {/*add classname={styles["captcha-refresh"]} both are located in Rules.module.css*/}
+        </div>
+      </Rule>
     );
   },
-  function rule12(input) {
+  function Rule12(input) {
     const reg =
       /(He|Li|Be|Ne|Na|Mg|Al|Si|Cl|Ar|Ca|Sc|Ti|Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|Rb|Sr|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|Xe|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|Fr|Ra|Ac|Th|Pa|Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Db|Sg|Bh|Hs|Mt|Ds|Rg|Cn|Fl|Lv|Ts|Og)/g;
     return (
@@ -74,7 +113,7 @@ const rulesList = [
       />
     );
   },
-  function rule14(input) {
+  function Rule14(input) {
     const reg =
       /(January|February|March|April|May|June|July|August|September|October|November|December)/gi;
     return (
